8f7dbe8d13ff8dc0b399d1c75b51b044e09307d6
assistantolegamicrosoft-droid <assistantolegamicrosoft@gmail.com>
2026-05-12 00:05:35 +0300

    111
    1

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+['change', 'click'].forEach(evt => {
+    document.addEventListener(evt, CheckFilter);
+});
+
+function CheckFilter() {
+    var type = document.getElementById('sort').value;
+    console.log(type);
+
+
+    const naturals = document.querySelectorAll('[data-category="Nat"]');
+    const humanits = document.querySelectorAll('[data-category="Hum"]');
+    const others = document.querySelectorAll('[data-category="Oth"]');
+    var N, H, O;
+    if (type == "Hum") {
+        H = '';
+        N = 'none';
+        O = 'none';
+    }
+    else if (type == "Oth") {
+        H = 'none';
+        N = 'none';
+        O = '';
+    }
+    else if (type == "Nat") {
+        H = 'none';
+        N = '';
+        O = 'none';
+    }
+    naturals.forEach(natur => {
+        natur.style.display = N;
+    });
+    humanits.forEach(human => {
+        human.style.display = H;
+    });
+    others.forEach(other => {
+        other.style.display = O;
+    })
+
+}
