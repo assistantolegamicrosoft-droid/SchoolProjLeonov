473565e27fe5fd16349fc340fe6e7dfccd96484c
assistantolegamicrosoft-droid <assistantolegamicrosoft@gmail.com>
2026-05-14 21:05:14 +0300

    1
    Поиск

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,23 @@
     document.addEventListener(evt, CheckFilter);
 });
 
+var type = document.getElementById('sort').value;
+const naturals = document.querySelectorAll('[data-category="Nat"]');
+const humanits = document.querySelectorAll('[data-category="Hum"]');
+const others = document.querySelectorAll('[data-category="Oth"]');
+var N, H, O;
+// Сбор values-----------------------------------------------
+const parentDiv = document.getElementById('cardsGrid');
+
+const divsWithValue = parentDiv.querySelectorAll('div[value]');
+
+const valuesArray = Array.from(divsWithValue).map(div => div.getAttribute('value'));
+// Переменная, хранящая текст, вводимый в поисковую строку
+let InputText = "";
+
+var input = document.getElementById("searchInput");
+//----------------------------------------------------------
 function CheckFilter() {
-    var type = document.getElementById('sort').value;
-    console.log(type);
-
-
-    const naturals = document.querySelectorAll('[data-category="Nat"]');
-    const humanits = document.querySelectorAll('[data-category="Hum"]');
-    const others = document.querySelectorAll('[data-category="Oth"]');
-    var N, H, O;
     if (type == "All") {
         H = '';
         N = '';
@@ -31,6 +39,7 @@ function CheckFilter() {
         N = '';
         O = 'none';
     }
+
     naturals.forEach(natur => {
         natur.style.display = N;
     });
@@ -40,5 +49,28 @@ function CheckFilter() {
     others.forEach(other => {
         other.style.display = O;
     })
-
 }
+
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        const InputText = String(input.value);
+        const len = InputText.length;
+
+        for (let i = 0; i < 6; i++) {
+            let count = 0;
+            const id = String(i);
+            for (let j = 0; j < len; j++) {
+                if (InputText[j] == valuesArray[i][j]) {
+                    count++;
+                }
+            }
+            console.log(count);
+            const displayStyle = (count === len) ? '' : 'none';
+            const el = document.getElementById(id);
+            if (el) {
+                el.style.display = displayStyle;
+            }
+        }
+    }
+});
+
