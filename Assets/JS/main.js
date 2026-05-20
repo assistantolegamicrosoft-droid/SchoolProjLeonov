79af8c6c61f8dd0b78e55b1af43644a83b42eff6
assistantolegamicrosoft-droid <assistantolegamicrosoft@gmail.com>
2026-05-20 17:36:51 +0300

    Alex
    1

---
+++ Assets/JS/main.js
@@ -0,0 +1,76 @@
+['change', 'click'].forEach(evt => {
+    document.addEventListener(evt, CheckFilter);
+});
+
+var type = document.getElementById('sort').value;
+const naturals = document.querySelectorAll('[data-category="Nat"]');
+const humanits = document.querySelectorAll('[data-category="Hum"]');
+const others = document.querySelectorAll('[data-category="Oth"]');
+var N, H, O;
+// Сбор values-----------------------------------------------
+const parentDiv = document.getElementById('cardsGrid');
+
+const divsWithValue = parentDiv.querySelectorAll('div[value]');
+
+const valuesArray = Array.from(divsWithValue).map(div => div.getAttribute('value'));
+// Переменная, хранящая текст, вводимый в поисковую строку
+let InputText = "";
+
+var input = document.getElementById("searchInput");
+//----------------------------------------------------------
+function CheckFilter() {
+    if (type == "All") {
+        H = '';
+        N = '';
+        O = '';
+    }
+    else if (type == "Hum") {
+        H = '';
+        N = 'none';
+        O = 'none';
+    }
+    else if (type == "Oth") {
+        H = 'none';
+        N = 'none';
+        O = '';
+    }
+    else if (type == "Nat") {
+        H = 'none';
+        N = '';
+        O = 'none';
+    }
+
+    naturals.forEach(natur => {
+        natur.style.display = N;
+    });
+    humanits.forEach(human => {
+        human.style.display = H;
+    });
+    others.forEach(other => {
+        other.style.display = O;
+    })
+}
+
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        const InputText = String(input.value);
+        const len = InputText.length;
+
+        for (let i = 0; i < 7; i++) {
+            let count = 0;
+            const id = String(i);
+            for (let j = 0; j < len; j++) {
+                if (InputText[j] == valuesArray[i][j]) {
+                    count++;
+                }
+            }
+            console.log(count);
+            const displayStyle = (count === len) ? '' : 'none';
+            const el = document.getElementById(id);
+            if (el) {
+                el.style.display = displayStyle;
+            }
+        }
+    }
+});
+
